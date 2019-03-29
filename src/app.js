626d2c5213672ea4660af078e93c3367732da89b
unknown <n305.net@gmail.com>
2019-03-29 08:57:51 -0400

    Implemented per test requirements.

--- src/app.js
+++ src/app.js
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './styles/app.css';
-import Task  from './components/task.component';
+import './styles/app.scss';
+import Task from './components/task.component';
+import 'typeface-roboto';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={todoArr:[], inputValue:''};
+    this.state = { todoArr: [], inputValue: '' };
     this.arr = [];
     this.todoNum = 0;
   }
 
-  handleOnchange(event){
-    const inputVal =  event.target.value;
-    this.setState(()=>{return {inputValue:inputVal}});
+  handleOnchange(event) {
+    const inputVal = event.target.value;
+    this.setState(() => { return { inputValue: inputVal } });
   }
 
   removeElement = (elm) => {
-    const arrCopy = this.state.todoArr.filter((datum, idx, arr)=>{
+    const arrCopy = this.state.todoArr.filter((datum, idx, arr) => {
       return datum.key !== elm.key;
     });
-    this.setState(()=>{return {todoArr: arrCopy}});
+    this.setState(() => { return { todoArr: arrCopy } });
   }
 
-  addTodo = ()=>{
+  addTodo = () => {
     const todoText = this.state.inputValue.trim();
-    if(todoText){
+    if (todoText) {
       this.todoNum++;
       const idKey = 'todo' + this.todoNum.toString();
-      const todoObj = {"key":idKey, "todo":todoText}
+      const todoObj = { "key": idKey, "todo": todoText }
       let arr = this.state.todoArr.slice();
       arr.push(todoObj);
-      this.setState(()=> {return {todoArr: arr}} );
-      this.setState(()=>{return {inputValue:''}});
+      this.setState(() => { return { todoArr: arr } });
+      this.setState(() => { return { inputValue: '' } });
     }
-  }
-
-  returnList = () => {
-    return this.state.todoArr;
   }
 
   render() {
     return (
       <div className="app">
-        <header className="app-header">
+        <div className="app-div">
           <img src={logo} className="app-logo" alt="logo" />
-          <input type="text" className="app-input" value={this.state.inputValue} onChange={(e)=>this.handleOnchange(e)} autoFocus/>      
-          <input className ="app-add" type="button" value="New" onClick={this.addTodo} />
-            {this.state.todoArr.map((elm)=>{return(<div onClick={(e)=> this.removeElement(elm)}><Task  key={elm.key} taskText={elm.todo}/></div>)})}
-        </header>
+          <input type="text" className="app-input" value={this.state.inputValue} onChange={(e) => this.handleOnchange(e)} autoFocus />
+          <button className="app-add-value" onClick={this.addTodo}>
+            <span>New</span>
+            <span className="app-add"> + </span>
+          </button>
+          {this.state.todoArr.map((elm) => { return (<div onClick={(e) => this.removeElement(elm)}><Task key={elm.key} taskText={elm.todo} /></div>) })}
+        </div>
       </div>
     );
   }
--- src/components/task.component.js
+++ src/components/task.component.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../styles/task.component.css';
+import '../styles/task.component.scss';
+import 'typeface-roboto';
 
 class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showCancel: false };
+  }
+
+  onMouseEnter = () => {
+    this.setState(() => { return { showCancel: true }; });
+  }
+
+  onMouseLeave = () => {
+    this.setState(() => { return { showCancel: false }; });
+  }
+
   render() {
     return (
-      <div className="task">
-      <input className ="task-button" type="button" value={this.props.taskText}/>
+      <div className="task" onMouseEnter={() => this.onMouseEnter()} onMouseLeave={() => this.onMouseLeave()}>
+        <button className="task-button">
+          <span className="task-value">{this.props.taskText}</span>
+          <span className={this.state.showCancel ? 'task-cancel' : 'task-cancel-clr'}>x</span>
+        </button>
       </div>
     );
   }
